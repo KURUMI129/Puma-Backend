82ef00fcabdccb691a7f0f2d55588671156938ba
Karol Levi Tafolla Salazar <karollevitafollasalazar@gmail.com>
2025-11-06 10:42:10 -0600

    Primer commit: Archivos iniciales del proyecto

---
+++ BD_Ejercicio.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE Articulos;
+USE Articulos;
+CREATE TABLE Clientes(
+    idCliente SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(40) NOT NULL,
+    Apellido VARCHAR(40) NOT NULL
+    Domicilio VARCHAR(40) NOT NULL,
+    Telefono VARCHAR(10) NOT NULL,
+    Correo VARCHAR(60) NOT NULL,
+    Contraseña VARCHAR(16) NOT NULL,
+    FechaNacimiento DATE NOT NULL,
+    Sexo VARCHAR(2) NOT NULL
+);
+
+CREATE TABLE Articulos(
+    idArtiuclo SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+    Descripcion VARCHAR(80) NOT NULL,
+    Precio FLOAT UNSIGNED NOT NULL,
+    Foto VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Compras(
+    idCompra SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Fecha DATE DEFAULT "2025/11/03", 
+    idCliente SMALLINT NOT NULL,
+    idArtiuclo SMALLINT NOT NULL AUTO_INCREMENT,
+    FOREIGN KEY (idCliente) REFERENCES Clientes (idCliente),
+    FOREIGN KEY (idArtiuclo) REFERENCES Articulos (idArtiuclo)
+);
+
+CREATE TABLE DetallesCompra(
+    idDetalle SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idCliente SMALLINT NOT NULL AUTO_INCREMENT,
+    idArtiuclo SMALLINT NOT NULL AUTO_INCREMENT,
+    idCompra SMALLINT NOT NULL AUTO_INCREMENT,
+    Cantidad SMALLINT NOT NULL
+    FOREIGN KEY (idCliente) REFERENCES Clientes (idCliente),
+    FOREIGN KEY (idArtiuclo) REFERENCES Articulos (idArtiuclo),
+    FOREIGN KEY (idCompra) REFERENCES Compras (idCompra) 
+);
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // Uso de la funcion
+app.use(express.json());
+
+const db = mysql.createConnection({ //Funcion para conectar
+    host: 'localhost',
+    user: 'root',
+    password: 'carlitos2541',
+    port: 3306,
+    database: 'Articulos'
+});
+
+db.connect(err => { // Controlar los errores
+    if (err) {
+        console.err('Error de conexion a la BD', err.stack);
+        return;
+    }
+    console.log('Conectado a la DB: ' + db.threadId);
+});
+
+//endpoint
+app.get('/OArt', (req, res) => {
+    console.log('Ruta del endpoint accedida');
+    db.query('SELECT * FROM Articulos', (err, result) => {
+        if(err) {
+            console.error(err);
+            console.error('Error de base de datos', err);
+            return res.status(500).json({
+                err: 'Error al consultar la base de datos. Intentalo mas tarde'
+            });
+        }
+        res.status(200).json(result);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server Running")
+});
